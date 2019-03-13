9f938506ed4bf8d7e5f3fba7e978f0617e3889b4
shaybelo <45208596+shaybelo@users.noreply.github.com>
2019-03-13 22:53:03 +0200

    navbar commit

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+ class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <input placeholder="search" id="searchbox" required></input>
+      </div>
+    );
+  }
+}
+
+export default Navbar
+
