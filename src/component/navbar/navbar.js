25765779d6b0c0e5cdb1715d527fb56a71e627fb
BmAlkes <41919686+BmAlkes@users.noreply.github.com>
2019-04-02 18:13:24 +0300

    test2

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
-import { Logo } from './logo';
+import Button from 'react-bootstrap/Button';
+
 
  class Navbar extends Component {
   render() {
     return (
-     <div>
-     <h1 className="header">
+
+     <div className="header">
        <a href="https://www.facebook.com/?ref=logo"className="Logo">
        
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/900px-Facebook_logo_%28square%29.png">
         </img>      
       </a>  
-      <img src="https://img.icons8.com/ios-glyphs/30/000000/search.png" className="searchbutton"></img>
-      </h1>       
-       </div>
+      <img src="http://icons.iconarchive.com/icons/icons8/ios7/256/Very-Basic-Search-icon.png" className="searchbutton"></img>
+      <div class="nav1">
+        <ul>
+          <li className="Button">
+          <Button href="http://www.facebook.com" bsStyle="primary">Home</Button>
+          </li>
+          <li>
+          <Button href="#" bsStyle="primary">Initial</Button>
+          </li>
+          <li>
+          <Button href="" bsStyle="primary">Create</Button>
+          </li>
+        </ul>
+      
+    
+        
+
+
+  
+  
+ 
+
+
+  
+  
+
+
+ 
+
+      </div> 
+      </div>
+      
+
+    
+
+
+
     );
   }
 }
